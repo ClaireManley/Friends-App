548308930a625445db3372c0f84af789f4898bec
Claire Manley <cmanley2@zagmail.gonzaga.edu>
2020-01-27 10:32:03 -0800

    Initial Commit

---
+++ Friends App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Friends App
+//
+//  Created by Claire Manley on 1/27/20.
+//  Copyright © 2020 Claire Manley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
