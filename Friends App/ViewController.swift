6bb9afaeb7c78d932b3144b7cd7823bd24172f04
Claire Manley <cmanley2@zagmail.gonzaga.edu>
2020-01-28 09:30:27 -0800

    This is app 2!

--- Friends App/ViewController.swift
+++ Friends App/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        imageView.image = UIImage(named: "image1")
+    }
+    
 
 }
 
